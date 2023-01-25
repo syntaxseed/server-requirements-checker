0cc0beaf27a81dcb83559f73375dd73b0067f260
Syntaxseed <825423+syntaxseed@users.noreply.github.com>
2023-01-25 14:45:08 -0500

    Update WordPress ruleset to check for apache modules.

--- index.php
+++ index.php
@@ -27,14 +27,18 @@ switch ($serverChecker->choice) {
         break;
     case 'wordpress':
         $serverChecker->checker->requirePhpVersion('>=8.0')
-            ->requirePhpExtensions(['Core', 'curl', 'dom', 'exif', 'fileinfo', 'hash', 'imagick', 'json', 'mbstring', 'mysqli', 'openssl', 'pcre', 'pdo', 'sodium', 'xml', 'zip'])
-            ->requireFunctions(['random_bytes'])
+            ->requirePhpExtensions([
+                'Core', 'curl', 'dom', 'exif', 'fileinfo', 'hash', 'imagick', 'json', 'mbstring', 'mysqli', 'openssl', 'pcre', 'pdo', 'sodium', 'xml', 'zip'
+            ])
+            ->requireFunctions([
+                'set_time_limit', 'ignore_user_abort'
+            ])
             ->requireIniValues([
                 'allow_url_fopen' => true,
                 'memory_limit'  => '>=64M',
             ])
             ->requireApacheModules([
-                'mod_rewrite'
+                'mod_alias', 'mod_authz_host', 'mod_deflate', 'mod_dir', 'mod_expires', 'mod_headers', 'mod_mime', 'mod_rewrite', 'mod_log_config', 'mod_autoindex', 'mod_negotiation', 'mod_setenvif'
             ]);
         break;
     case 'basic':
